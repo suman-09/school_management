51f8818020c8eb9592ef247332103702d0a56951
Suman Karmakar <sumankarmakar@Sumans-MacBook-Air.local>
2023-01-27 01:30:53 +0530

    created route for saving students and to fetch the json data

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv");
-dotenv.config({path: "../.env"})
+const Student = require("./models/students");
+dotenv.config({path: "../.env"});
 
 require("./db/conn")
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(express.json())  // use this as to get json response
 
 // create a new student
 
@@ -13,7 +16,14 @@ app.get("/", (req,res) => {
 })
 
 app.post("/students", (req,res) => {
-    res.send("hello from student");
+    
+    const user = new Student(req.body);
+    user.save().then(() => {
+        res.status(201).send(user);
+    }).catch((e) => {
+        res.status(400).send(e);
+    })
+
 })
 
 app.listen(port, () =>{
