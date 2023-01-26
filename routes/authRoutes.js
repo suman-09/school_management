76f5c0003ae5b2b70ef1428b8c5b6147537bb466
Suman Karmakar <sumankarmakar@Sumans-MacBook-Air.local>
2023-01-27 03:15:04 +0530

    added route for registering teachers

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,5 +4,6 @@ const authController = require("../controllers/authControllers");
 const router = Router();
 
 router.post("/students", authController.student_register);
+router.post("/teachers", authController.teacher_register);
 
 module.exports = router;
