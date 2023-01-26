5ce731f9f445640d19c08dc738962aad8afcef2c
Suman Karmakar <sumankarmakar@Sumans-MacBook-Air.local>
2023-01-27 03:14:10 +0530

    updated schema for teachers

--- models/teacher.js
+++ models/teacher.js
@@ -7,5 +7,27 @@ const teacherSchema = new mongoose.Schema({
         minlength: 2,
         required: true
     },
-    
+    email: {
+        type: String,
+        required: true,
+        unique: [true, "email is already exists"],
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error("Invalid Email")
+            }
+        }
+    },
+    phone: {
+        type: Number,
+        min: 10,
+        required: true,
+        unique: true
+    },
+    address: {
+        type: String,
+        required: true
+    }
 })
+
+const Teacher = new mongoose.model('Teacher', teacherSchema);
+module.exports = Teacher;
