d0f831b0250e54e356435804b9ab429e61c470f2
Suman Karmakar <sumankarmakar@Sumans-MacBook-Air.local>
2023-01-27 03:13:22 +0530

    added controller to regoster teacher

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,7 +1,19 @@
 const Student = require("../models/students");
+const Teacher = require("../models/teacher");
 
+// crating students
 module.exports.student_register = (req,res) => {
     const user = new Student(req.body);
+    user.save().then(() => {
+        res.status(201).send(user);
+    }).catch((e) => {
+        res.status(400).send(e);
+    })
+}
+
+// creating teachers
+module.exports.teacher_register = (req,res) => {
+    const user = new Teacher(req.body);
     user.save().then(() => {
         res.status(201).send(user);
     }).catch((e) => {
